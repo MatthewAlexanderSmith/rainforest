2251da350746d347b8c7d0a519d790032a9ae6da
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-02-06 16:04:14 -0500

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   has_many :products, through: :reviews
 
-  validates :name, presence: true
-
-
+  # validates :first_name, presence: true
 
 end
